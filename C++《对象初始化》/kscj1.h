21989b73de47fe4a4e2338fad59d3f19080487da
Sunjiumeng <110646628+Sunjiumeng@users.noreply.github.com>
2022-08-07 10:11:04 +0800

    Create kscj1.h
    声明类

---
+++ C++《对象初始化》/kscj1.h
@@ -0,0 +1,8 @@
+#include <iostream>
+	class Point;        //Point 类声明
+	class TPoint;		//TPoint 类声明
+	class Part;			//Part 类声明
+	class Whole;		//Whole 类声明
+	class Heapclass;	//Heapclass 类声明
+	class Derivedclass;	//Derivedclass 类声明
+	class Baseclass;	//Baseclass 类声明
