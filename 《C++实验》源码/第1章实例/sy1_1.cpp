4eeaf6f01e53868694d76aaa6be41768017d3e8c
Sunjiumeng <110646628+Sunjiumeng@users.noreply.github.com>
2022-10-10 08:45:30 +0800

    Delete 《C++实验》源码 directory

--- 《C++实验》源码/第1章实例/sy1_1.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream.h>
-int main()
-{ 
-cout<<"this is the start of something!"<<endl;
-return 0;
-}
--- 《C++实验》源码/第2章实例/Li2_1.cpp
+++
@@ -1,8 +0,0 @@
-//≥Ã–ÚLi2_1.cpp
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout<<"hello,students!"<<endl;
-	return 0;
-}
